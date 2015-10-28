e287dd24a015f19a147d007acb57ebe3e05c1c24
Christian <me@rndm.de>
2015-10-28 11:04:44 +0100

    adds support for non deprecated `application.resolveRegistration`

--- addon/initializers/ember-validations.js
+++ addon/initializers/ember-validations.js
@@ -3,8 +3,9 @@ import Ember from 'ember';
 
 const {extend} = Ember.$;
 
-export function initialize(registry) {
-  const ENV = registry.resolve('config:environment');
+export function initialize(application) {
+  const ENVIRONMENT_REGISTRATION_KEY = 'config:environment';
+  const ENV = application.resolveRegistration ? application.resolveRegistration(ENVIRONMENT_REGISTRATION_KEY) : application.resolve(ENVIRONMENT_REGISTRATION_KEY);
   if (ENV.hasOwnProperty('ember-validations') && ENV['ember-validations'].hasOwnProperty('messages')) {
     messages.defaults = extend({}, messages.defaults, ENV['ember-validations'].messages);
   }
